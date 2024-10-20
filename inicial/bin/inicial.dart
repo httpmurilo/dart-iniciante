2ea632f2be41987b8988a447618d6b930fd1c808
httpmurilo <muriloeduardo1997@outlook.com>
2024-10-20 09:13:59 -0300

    ok

---
+++ inicial/bin/inicial.dart
@@ -0,0 +1,5 @@
+import 'package:inicial/inicial.dart' as inicial;
+
+void main(List<String> arguments) {
+  print('Hello world: ${inicial.calculate()}!');
+}
---
+++ inicial/test/inicial_test.dart
@@ -0,0 +1,8 @@
+import 'package:inicial/inicial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
